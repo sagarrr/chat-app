492f7b9dde0df0b75ab7f2ee0cd0423e195582d9
root <root@localhost.localdomain>
2017-03-16 17:41:55 +0530

    adding files

---
+++ public/js/app.js
@@ -0,0 +1,103 @@
+var app = angular.module('chatApp', ['btford.socket-io', 'ngAnimate']);
+
+app.directive('newMessage', function(){
+    return{
+        restrict: 'E',
+        template: '<p><strong>{{messageObj.name}}</strong>: {{messageObj.content}} <small>- {{messageObj.time}}</small></p>',
+        scope: {
+            messageObj: '='
+        },
+        replace: true
+    }
+});
+
+app.factory('socket', ['socketFactory', function(socketFactory){
+    return socketFactory();
+}]);
+
+app.controller('MainController', ['socket', '$scope', '$timeout', function(socket, $scope, $timeout){
+
+    $scope.nameSelected = false;
+    $scope.showChat = false;
+    $scope.messages = [];
+
+    var chatWindow = document.getElementById('chatwindow');
+
+    socket.on('connect', function(){
+        console.log('Connected to socket.io server');
+    });
+
+    socket.on('message', function(message){
+        console.log('New message');
+        console.log(message.content);
+        console.log(message.timestamp);
+
+        message.time = moment.utc(message.timestamp).local().format('h:mm a');
+
+        $scope.messages.push(message);
+
+        $timeout(function(){
+            chatWindow.scrollTop = chatWindow.scrollHeight;
+        });
+    });
+
+    socket.on('nudge', function(){
+        angular.element("#chatpanel").animate("tada");
+    });
+
+    $scope.myMessage = {
+        name: '',
+        content: ''
+    }
+
+    $scope.$watch('myMessage.content', function(){
+        if($scope.myMessage.content !== ''){
+            $scope.sendButton = 'Send';
+        }else{
+            $scope.sendButton = 'Nudge';
+        }
+    });
+
+    $scope.sendButtonFunc = function(){
+        if($scope.myMessage.content !== ''){
+            $scope.sendMessage();
+        }else{
+            socket.emit('nudge');
+        }
+    }
+
+    $scope.messageBoxEnter = function(){
+        if($scope.myMessage.content !== ''){
+            $scope.sendMessage();
+        }
+    }
+
+
+    $scope.sendMessage = function(){
+        if($scope.myMessage.content !== ''){
+            socket.emit('message', $scope.myMessage);
+            $scope.myMessage.content = '';
+        }
+    }
+
+    $scope.setName = function(){
+        if($scope.myMessage.name !== ''){
+            $scope.nameSelected = true;
+            socket.emit('setname', $scope.myMessage.name);
+            $timeout(function () {
+                $scope.showChat = true;
+            }, 1000);
+
+        }else{
+            angular.element("#namepanel").animate("shake");
+        }
+    }
+
+    angular.element.prototype.animate = function (animationName) {
+        var self = this;
+        self.addClass("animated " + animationName);
+        self.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+            self.removeClass("animated " + animationName);
+        });
+    }
+}]);
---
+++ server.js
@@ -0,0 +1,64 @@
+var PORT = process.env.PORT || 3000;
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var moment = require('moment');
+
+app.use(express.static(__dirname + '/public'));
+
+var clientInfo = {};
+
+io.on('connection', function(socket) {
+    console.log('User connected via socket.io');
+
+    socket.on('disconnect', function(){
+        if(clientInfo[socket.id] !== 'undefined'){
+            io.emit('message', {
+                content: clientInfo[socket.id] + ' left the chat',
+                name: 'Bot',
+                timestamp: moment().valueOf()
+            })
+        }
+    })
+
+    socket.on('message', function(message) {
+        console.log('Message recieved: ' + message.content);
+
+        message.timestamp = moment().valueOf();
+
+        console.log(message.timestamp);
+        io.emit('message', message);
+    });
+
+    socket.on('setname', function(name){
+        clientInfo[socket.id] = name;
+
+        socket.emit('message', {
+            content: 'Welcome to this chat app, ' + clientInfo[socket.id] + '!',
+            name: 'Welcome',
+            timestamp: moment().valueOf()
+        });
+
+        io.emit('message', {
+            content: clientInfo[socket.id] + ' joined the chat!',
+            name: 'Bot',
+            timestamp: moment().valueOf()
+        })
+
+        console.log(clientInfo[socket.id]);
+    });
+
+    socket.on('nudge', function(){
+        console.log('Nudge recieved!');
+
+        io.emit('nudge');
+    });
+
+
+
+});
+
+http.listen(PORT, function(){
+    console.log('Listening on 3000...');
+});
